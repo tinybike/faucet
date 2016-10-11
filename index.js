574c88ef0eb4afaada2c04836c257445e59fd53d
Jack Peterson <jack@tinybike.net>
2016-10-11 04:23:29 -0700

    Simple spam check

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ var connectInfo = {
     ipc: process.env.GETH_IPC || join(DATADIR, "geth.ipc")
 };
 augur.connect(connectInfo);
-augur.rpc.debug.broadcast = true;
-augur.rpc.retryDroppedTxs = true;
 
 app.get("/", function (req, res) {
     res.end("How about a free lunch?");
@@ -32,8 +30,33 @@ app.get("/faucet", function (req, res) {
     res.end("How about a free lunch?");
 });
 
+var hasAlreadySentTo = {};
+var lastReqTime = {};
+var baddies = {};
+var blacklist = {};
+
 app.get("/faucet/:address", function (req, res) {
+    var ip = req.headers['x-forwarded-for'] || 
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    if (blacklist[ip]) return res.end(":(");
+    if (req.params.address.length < 40) return res.end(":(");
+    var curTime = new Date().getTime();
+    var prevTime = lastReqTime[ip];
+    console.log(req.params.address, ip, lastReqTime[ip], curTime - prevTime, baddies[ip]);
+    lastReqTime[ip] = curTime;
+    if (prevTime && curTime - prevTime < 60000) {
+        baddies[ip] = (!baddies[ip]) ? 1 : baddies[ip] + 1;
+        if (baddies[ip] > 10) {
+            console.log('Blacklisted IP', ip);
+            blacklist[ip] = true;
+        }
+        return res.end(":(");
+    }
     var address = abi.format_address(req.params.address);
+    if (hasAlreadySentTo[address]) return res.end("Already sent Ether to " + address);
+    hasAlreadySentTo[address] = true;
     if (!augur.rpc.ipcpath) augur.connect(connectInfo);
     augur.rpc.balance(address, function (balance) {
         balance = new BigNumber(balance).dividedBy(ETHER);
@@ -43,27 +66,32 @@ app.get("/faucet/:address", function (req, res) {
                 augur.coinbase,
                 fs.readFileSync(join(DATADIR, ".password")).toString("utf8")
             ], function (unlocked) {
-                if (unlocked && unlocked.error) return res.end("Couldn't unlock Ethereum node.");
+                if (unlocked && unlocked.error) {
+                    hasAlreadySentTo[address] = false;
+                    return res.end("Couldn't unlock Ethereum node.");
+                }
                 augur.rpc.sendEther({
                     to: address,
                     value: etherToSend.toFixed(),
                     from: augur.coinbase,
                     onSent: function (r) {
-                        console.log("sendEther sent:", r);
                         augur.rpc.personal("lockAccount", [augur.coinbase], function (locked) {
                             if (locked && locked.error) {
                                 console.error("lockAccount failed:", locked);
                                 augur.connect(connectInfo);
+                                hasAlreadySentTo[address] = false;
                             }
                         });
                     },
                     onSuccess: function (r) {
                         console.log("sendEther succeeded:", r);
                         res.end("Sent " + etherToSend.toFixed() + " ether to " + address + ".");
+                        hasAlreadySentTo[address] = true;
                     },
                     onFailed: function (e) {
                         console.error("sendEther failed:", e);
                         res.end("Couldn't send ether to " + address + ".");
+                        hasAlreadySentTo[address] = false;
                         augur.connect(connectInfo);
                         augur.rpc.balance(augur.coinbase, function (balance) {
                             balance = new BigNumber(balance, 16).dividedBy(ETHER);
@@ -75,6 +103,7 @@ app.get("/faucet/:address", function (req, res) {
                                 if (locked && locked.error) {
                                     console.log("lockAccount failed:", locked);
                                     augur.connect(connectInfo);
+                                    hasAlreadySentTo[address] = false;
                                 }
                             });
                         });
